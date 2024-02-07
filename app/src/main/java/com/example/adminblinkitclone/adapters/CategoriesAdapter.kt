0702e9a8101d98aedb0024f911f4722963aefa6d
Shubhansh16 <shubhansh77@gmail.com>
2024-02-07 22:05:48 +0530

    "Completed the HomeFragment.kt and made changes adapters/CategoriesAdapter.kt and AdminViewModel.kt"

--- app/src/main/java/com/example/adminblinkitclone/adapters/CategoriesAdapter.kt
+++ app/src/main/java/com/example/adminblinkitclone/adapters/CategoriesAdapter.kt
@@ -3,12 +3,12 @@ package com.example.adminblinkitclone.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.adminblinkitclone.databinding.ItemViewImageSelectionBinding
 import com.example.adminblinkitclone.databinding.ItemViewProductCategoriesBinding
 import com.example.adminblinkitclone.models.Categories
 
 class CategoriesAdapter(
     private val categoryArrayList: ArrayList<Categories>,
+   val  onCategoryClicked: (Categories) -> Unit,
 ): RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder>() {
 
     class CategoriesViewHolder(val binding: ItemViewProductCategoriesBinding):RecyclerView.ViewHolder(binding.root)
@@ -26,6 +26,9 @@ class CategoriesAdapter(
         holder.binding.apply {
             ivCategoryImage.setImageResource(category.icon)
             tvCategoryTitle.text=category.category
+        }
+        holder.itemView.setOnClickListener {
+            onCategoryClicked(category)
         }
     }
 }
--- app/src/main/java/com/example/adminblinkitclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/adminblinkitclone/fragments/HomeFragment.kt
@@ -7,16 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.adminblinkitclone.Constants
 import com.example.adminblinkitclone.R
+import com.example.adminblinkitclone.adapters.AdapterProduct
 import com.example.adminblinkitclone.adapters.CategoriesAdapter
 import com.example.adminblinkitclone.databinding.FragmentHomeBinding
 import com.example.adminblinkitclone.models.Categories
+import com.example.adminblinkitclone.viewmodel.AdminViewModel
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private val viewModel: AdminViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,15 +31,45 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         setStatusBarColor()
         setCategories()
+        getAllTheProducts("All")
         return binding.root
+    }
+
+    private fun getAllTheProducts(category: String) {
+        lifecycleScope.launch {
+            viewModel.fetchAllProducts(category).collect{
+
+                if (it.isEmpty()){
+                    binding.rvProducts.visibility =View.GONE
+                    binding.tvText.visibility =View.VISIBLE
+                }
+                else
+                {
+                    binding.rvProducts.visibility =View.VISIBLE
+                    binding.tvText.visibility =View.GONE
+                }
+                val adapterProduct = AdapterProduct()
+                binding.rvProducts.adapter = adapterProduct
+                adapterProduct.differ.submitList(it)
+            }
+        }
     }
 
     private fun setCategories() {
         val categoryList = ArrayList<Categories>()
-        for (i in 0 until Constants.allProductsCategoryIcon.size){
-            categoryList.add(Categories(Constants.allProductsCategory[i],Constants.allProductsCategoryIcon[i]))
+        val minSize = minOf(Constants.allProductsCategory.size, Constants.allProductsCategoryIcon.size)
+
+        // Iterate over the minimum size
+        for (i in 0 until minSize) {
+            // Access elements from both arrays if within valid index range
+            val category = Categories(Constants.allProductsCategory[i], Constants.allProductsCategoryIcon[i])
+            categoryList.add(category)
         }
-        binding.rvCategories.adapter=CategoriesAdapter(categoryList)
+        binding.rvCategories.adapter=CategoriesAdapter(categoryList, ::onCategoryClicked)
+    }
+
+    private fun onCategoryClicked(categories: Categories){
+       getAllTheProducts(categories.category)
     }
 
     private fun setStatusBarColor(){
